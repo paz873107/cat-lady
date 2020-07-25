6aa5feaf288371481b629b0ea663d2e138d235aa
paz873107 <60817728+paz873107@users.noreply.github.com>
2020-07-24 21:42:37 -0300

    Primeros pasos en Javascript

---
+++ 05.playground/READ ME.md
@@ -0,0 +1,3 @@
+First steps in Javascript, the task instructions are attached in the image files. Look at the console!
+
+Primeros pasos en Javascript. Las instrucciones de la tarea están adjuntas en las imágenes. Mira la consola!
--- 05.playground/main.js
+++ 05.playground/main.js
@@ -1,3 +1,4 @@
+// Playing with variables to log ages and the difference between two different values
 var MyName = "Paz";
 console.log(MyName);
 
@@ -8,6 +9,7 @@ var ignasiAge = 32;
 var ageDiff = age - ignasiAge;
 console.log(ageDiff);
 
+// First conditional ever
 var number = 21;
 if (age > 21) {
   console.log("you are older than 21");
@@ -15,7 +17,7 @@ if (age > 21) {
   console.log("you are not older than 21");
 }
 
-
+// More conditionals, more playing with basic math
 if (age > ignasiAge) {
   console.log("you are older than Ignasi");
 } else if (age < ignasiAge) {
@@ -24,8 +26,7 @@ if (age > ignasiAge) {
   console.log("you are the same age as Ignasi");
 }
 
-
-
+// .length and .sort 101
 var names = ["Priscila", "Germán", "Alana", "Octavio", "Rebecca"];
 console.log(names.length);
 names.sort();
@@ -33,13 +34,13 @@ console.log(names[0]);
 console.log(names[names.length - 1]);
 console.log(names);
 
-
+// My very first for loop
 for (let index = 0; index < names.length; index++) {
   console.log(names[index]);
 }
+
+// Logging even numbers using loops and conditionals
 var ages = [32, 18, 19, 22, 19, 17];
-
-
 function nom(a) {
   console.log(a.length);
   var i = 0;
@@ -52,18 +53,18 @@ function nom(a) {
 }
 nom(ages);
 
-
+// Logging the values of an array one by one
 for (let index = 0; index < ages.length; index++) {
   console.log(ages[index]);
 }
 
-
+// using the Math library to get the square root
 function hyp(a, b) {
   console.log(Math.sqrt(a * a + b * b));
 }
 hyp(3, 4);
 
-
+// getting the lowest number of the array
 function minimo(d) {
   var min = d[0];
   for (let index = 1; index < d.length; index++) {
@@ -75,7 +76,7 @@ function minimo(d) {
 }
 minimo(ages);
 
-
+//getting the biggest number of the array
 function maximo(d) {
   var max = d[0];
   for (let index = 1; index < d.length; index++) {
@@ -87,7 +88,7 @@ function maximo(d) {
 }
 maximo(ages);
 
-
+// Print the value at the given position
 var array = [5, 67, 75, 3, 100, 28];
 var index = 2;
 function whatever(array, j) {
@@ -101,31 +102,29 @@ whatever(array, index);
 
 
 
-
+// get only the repeated numbers
 function repetidos(array) {
   var array = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100];
   var dupl = [];
-for (var i = 0; i < array.length; i++) {
-  for (var x = i; x < array.length; x++) {
-    if (i!=x && array[i]==array[x]) {
-      dupl.push.apply(array[i])
-      console.log(array[i]);
+  for (var i = 0; i < array.length; i++) {
+    for (var x = i; x < array.length; x++) {
+      if (i!=x && array[i]==array[x]) {
+        dupl.push.apply(array[i])
+        console.log(array[i]);
+      }
+      
     }
-    
-  }
 
-}
+  }
 }
 repetidos(array);
 
-
-
-
+// turn the array into a string
 var myColor = ["Red", "Green", "White", "Black"]
 var x = myColor.toString()
 console.log(x);
 
-
+// reverse the string
 var i = "35";
 var reversed = "";
 function reverse(str) {
@@ -136,16 +135,14 @@ function reverse(str) {
 }
 reverse(i);
 
-
-function alphabet_order(str)
-  {
-return str.split('').sort().join('');
-  }
+// order alphabetically
+function alphabet_order(str){
+  return str.split('').sort().join('');
+}
 console.log(alphabet_order("supercalifragilistico"));
 
-
-function uppercase(str)
-{
+// turn the first letter of each word into upper case
+function uppercase(str){
   var array1 = str.split(' ');
   var newarray1 = [];
   for (var x = 0; x < array1.length; x++) {
@@ -156,15 +153,12 @@ function uppercase(str)
 console.log (uppercase("prince of persia"));
 
 
-
-function find_longest_word(str)
-{
+// Find the longest word in the array
+function find_longest_word(str){
   var array1 = str.match(/\w[a-z]{0,}/gi);
   var result = array1[0];
-  for(var x = 1 ; x < array1.length ; x++) 
-  {
-    if(result.length < array1[x].length) 
-    {
+  for(var x = 1 ; x < array1.length ; x++) {
+    if(result.length < array1[x].length) {
      result = array1[x];
     }
   }
