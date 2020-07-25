8974f8849d0476db183a0da584ac6760639f9e3d
paz873107 <60817728+paz873107@users.noreply.github.com>
2020-07-25 18:35:44 -0300

    comentarios añadidos, creado el read me, código emprolijado

--- 08.JS4/Main.js
+++ 08.JS4/Main.js
@@ -4,7 +4,6 @@ var app = new Vue({
         senates: [],
         house: [],
         statistics: {
-
             "democrats": [],
             "d_total": 0,
             "republicans": [],
@@ -20,10 +19,12 @@ var app = new Vue({
             "most_engaged": [],
             "least_loyal": [],
             "most_loyal": []
-
         }
     }
 });
+
+// This time, the result target is the same as the previous step in the project, but the functions are improved by using VUE,
+// promises and fetch to get live data and avoid the making of the tables in the js file
 
 let url1 = "https://api.propublica.org/congress/v1/113/senate/members.json"
 let url2 = "https://api.propublica.org/congress/v1/113/house/members.json"
@@ -35,25 +36,17 @@ function fSenate() {
         headers: new Headers({
             "X-API-Key": 'Gr0ZgB1BlfNDiL4ip2sgsvY06EikpZcxyhKylBV4'
         }),
-
     }).then(function (response) {
         if (response.ok)
             return response.json();
         throw new Error(response.statusText);
     }).then(data => {
         app.senates = data.results[0].members
-        console.log(app.senates);
         calculateAttendance(app.senates)
         calculateAverage()
         Attendance(app.senates, 'missed_votes', app.statistics.most_engaged, app.statistics.least_engaged)
-
-       
-        Attendance(app.senates, 'total_votes', app.statistics.least_loyal, app.statistics.most_loyal)
-        
-        console.log(app.statistics.total_avg)
-        console.log(app.statistics.total)
+        Attendance(app.senates, 'total_votes', app.statistics.least_loyal, app.statistics.most_loyal)       
     });
-
 }
 
 
@@ -69,16 +62,10 @@ function fHouse() {
         throw new Error(response.statusText);
     }).then(data => {
         app.house = data.results[0].members
-        console.log(app.house);
         calculateAttendance(app.house)
         calculateAverage()
         Attendance(app.house, 'missed_votes', app.statistics.most_engaged, app.statistics.least_engaged)
-       
         Attendance(app.house, 'total_votes', app.statistics.least_loyal, app.statistics.most_loyal)
-        
-        console.log(app.statistics.total_avg)
-        console.log(app.statistics.total)
-
     });
 }
 
@@ -94,31 +81,18 @@ function calculateAttendance(member) {
     app.statistics.i_total = app.statistics.independents.length;
 
     app.statistics.total = (app.statistics.democrats.length + app.statistics.republicans.length + app.statistics.independents.length)
-
-   
-    console.log(app.statistics.democrats)
-    
-    console.log(app.statistics.republicans)
-    
-    console.log(app.statistics.independents)
-
-    console.log(app.statistics.total)
 }
 
 
 
 function calculateAverage() {
-
     if (app.statistics.democrats != 0) {
         app.statistics.avg_democrats = app.statistics.democrats.reduce(function (total, currentValue) {
             return total + currentValue
         })
         app.statistics.total_avg = (app.statistics.total_avg + app.statistics.avg_democrats)
-      
-        console.log(app.statistics.total_avg)
         app.statistics.avg_democrats = (app.statistics.avg_democrats / app.statistics.democrats.length).toFixed(2);
     }
-
 
     if (app.statistics.republicans != 0) {
         app.statistics.avg_republicans = app.statistics.republicans.reduce(function (total, currentValue) {
@@ -126,11 +100,8 @@ function calculateAverage() {
         })
         app.statistics.total_avg = (app.statistics.total_avg + app.statistics.avg_republicans)
         
-        console.log(app.statistics.total_avg)
         app.statistics.avg_republicans = (app.statistics.avg_republicans / app.statistics.republicans.length).toFixed(2);
     }
-
-
 
     if (app.statistics.independents != 0) {
         app.statistics.avg_independents = app.statistics.independents.reduce(function (total, currentValue) {
@@ -138,38 +109,29 @@ function calculateAverage() {
         })
         app.statistics.total_avg = (app.statistics.total_avg + app.statistics.avg_independents)
         
-        console.log(app.statistics.total_avg)
+
         app.statistics.avg_independents = (app.statistics.avg_independents / app.statistics.independents.length).toFixed(2);
     }
 
-
-
     app.statistics.total_avg = (app.statistics.total_avg / app.statistics.total).toFixed(2);
-
-    console.log(app.statistics.total_avg)
-    console.log(app.statistics.total)
-
 }
 
 
 function Attendance(array, value, most, least) {
 
     var porcentaje = Math.round(app.statistics.total * 10 / 100);
-    console.log(porcentaje)
 
     function order(array, value) {
         array.sort(function (a, b) {
             return a[value] - b[value];
         });
-
     }
     order(array, value);
     for (i = 0; i < porcentaje; i++) {
         most.push(array[i])
     }
     most.forEach(element => {
-        console.log(element)
-
+        return(element)
     });
     
     array.reverse(array, value)
@@ -177,11 +139,7 @@ function Attendance(array, value, most, least) {
         least.push(array[i])
     }
     least.forEach(element => {
-        console.log(element)
-
+        return(element)
     });
 
 }
-
-
-console.log(app.statistics.total_avg)
